8f719c7e03a05389e2388ec336759e8f0e64e542
Chrish6 <christianishaq07@gmail.com>
2025-12-01 18:44:54 +0000

    1111

---
+++ Eget arbete jack and jones/main.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  var element = document.getElementById("m1");
+  element.classList.toggle("m1");
+}
+function toggleJJXX() {
+  var element = document.getElementById("m2");
+  element.classList.toggle("m2");
+}
+function toggleHelp() {
+  var element = document.getElementById("m3");
+  element.classList.toggle("m3");
+}
+function toggleBestceller() {
+  var element = document.getElementById("m4");
+  element.classList.toggle("m4");
+}
