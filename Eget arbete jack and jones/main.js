b93099afc07161d9eb6d862d039179d9e6a3b27f
Chrish6 <christianishaq07@gmail.com>
2025-12-12 20:29:11 +0000

    added product page and finished main kläder

--- Eget arbete jack and jones/main.js
+++ Eget arbete jack and jones/main.js
@@ -1,19 +1,39 @@
 function myFunction() {
   document.querySelector("#m1").classList.toggle("open");
 }
+
 function toggleJJXX() {
   document.querySelector("#m2").classList.toggle("open");
 }
+
 function toggleHelp() {
   document.querySelector("#m3").classList.toggle("open");
 }
+
 function toggleBestceller() {
   document.querySelector("#m4").classList.toggle("open");
 }
 
-
 function toggleMenu() {
   document.querySelector(".headerMobile").classList.toggle("visible")
+}
 
+function toggleXS() {
+  document.querySelector(".xs").classList.toggle("selected");
+}
 
+function toggleS() {
+  document.querySelector(".s").classList.toggle("selected");
+}
+
+function toggleM() {
+  document.querySelector(".m").classList.toggle("selected");
+}
+
+function toggleL() {
+  document.querySelector(".l").classList.toggle("selected");
+}
+
+function toggleXL() {
+  document.querySelector(".xl").classList.toggle("selected");
 }
