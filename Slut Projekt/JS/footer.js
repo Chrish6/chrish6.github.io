7533f91fe9700e41f0065306e33403bc3b8b6d49
Chrish6 <christianishaq07@gmail.com>
2026-03-27 20:45:11 +0100

    footer 50%

--- Slut Projekt/JS/footer.js
+++ Slut Projekt/JS/footer.js
@@ -1,2 +1,78 @@
 const el = document.querySelector("#footer");
 ReactDOM.createRoot(el).render(<Footer />);
+
+function Footer() {
+  return (
+    <footer>
+      
+        <div className="footer-kolumn">
+          <h3 onClick={toggleMeny}>
+            Onwheels <i className="fa-solid fa-angle-down"></i>
+          </h3>
+          <div className="droppDown">
+            <a href="#">Försäljningsvillkor</a>
+            <a href="#">Betalningsinformation</a>
+            <a href="#">Leveransinformation</a>
+            <a href="#">Fraktinformation</a>
+            <a href="#">Kontovillkor</a>
+            <a href="#">Integritetspolicy</a>
+            <a href="#">Cookiepolicy</a>
+          </div>
+        </div>
+
+        <div className="footer-kolumn">
+          <h3 onClick={toggleMeny} id="tjänster">
+            Tjänster <i className="fa-solid fa-angle-down"></i>
+          </h3>
+          <div className="droppDown">
+            <a href="#">Lackering</a>
+            <a href="#">Karosseri</a>
+            <a href="#">Rekonditionering</a>
+            <a href="#">Stenskottsskydd</a>
+            <a href="#">Vindrutebyte</a>
+            <a href="#">Däckbyte</a>
+            <a href="#">Biltvätt</a>
+          </div>
+        </div>
+
+        <div className="footer-kolumn">
+          <h3 onClick={toggleMeny}>
+            Kundservice <i className="fa-solid fa-angle-down"></i>
+          </h3>
+          <div className="droppDown">
+            <a href="#">Beställning</a>
+            <a href="#">Leverans</a>
+            <a href="#">Reklamation & återköp</a>
+            <a href="#">Teknisk support</a>
+            <a href="#">Betalning</a>
+            <a href="#">Kontakta oss</a>
+          </div>
+        </div>
+
+        <div className="footer-kolumn">
+          <h3 onClick={toggleMeny}>
+            Om Onwheels <i className="fa-solid fa-angle-down"></i>
+          </h3>
+          <div className="droppDown">
+            <a href="#">Om oss</a>
+            <a href="#">Karriär</a>
+            <a href="#">Hållbart val</a>
+            <a href="#">Våra varumärken</a>
+            <a href="#">Hitta butik</a>
+            <a href="#">Säkerhet och Integritet</a>
+          </div>
+        </div>
+      
+    </footer>
+  );
+}
+/* cloude hjälpte */
+function toggleMeny(e) {
+  const droppDown = e.currentTarget.nextElementSibling;
+  droppDown.classList.toggle("open");
+  e.currentTarget.querySelector("i").classList.toggle("open");
+}
+
+/* function toggleMeny(e) {
+  e.currentTarget.nextElementSibling.classList.toggle("open");
+} */
--- Slut Projekt/JS/header.js
+++ Slut Projekt/JS/header.js
@@ -9,7 +9,7 @@ function Header() {
           <span className="menuButton" id="menuButton" onClick={toggleNav}>
             <i className="fa-solid fa-bars"></i>
           </span>
-          <div className="logo" href="#">
+          <div className="logo" href="/">
             <img src="../IMG/Logo.png" alt="" />
           </div>
           <div className="nav-search">
@@ -36,14 +36,15 @@ function Header() {
           </div>
         </div>
         <div className="nav-links" id="navLinks">
-          <a href="#">Startsida</a>
           <a href="#">Reservdelar</a>
           <a href="#">Lackering</a>
           <a href="#">Karosseri</a>
+          <a href="#">Polering</a>
           <a href="#">Verktyg</a>
           <a href="#">Varumärken</a>
           <a href="#">Om oss</a>
-          <a href="#" className="highlight">Kontakta oss</a>
+          <a href="#">Kontakta oss</a>
+          <a href="#tjänster">Tjänster</a>
         </div>
       </nav>
     </header>
